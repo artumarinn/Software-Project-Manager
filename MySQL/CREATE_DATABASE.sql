e7c218e3a635290b2d633c263da2b8708efbe902
artumarinn <artumarin.09@gmail.com>
2024-06-17 15:41:45 -0300

    Registro y Bd nuevos

--- MySQL/CREATE_DATABASE.sql
+++ MySQL/CREATE_DATABASE.sql
@@ -4,79 +4,97 @@ USE sistemaconsultora;
 
 CREATE TABLE roles (
    id_rol INT AUTO_INCREMENT PRIMARY KEY,
-   Nombre_rol VARCHAR(100)
+   Descripcion VARCHAR(50) 
 );
 
-CREATE TABLE usuario(
-   id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-   Usuario VARCHAR(100),
-   Nombre_completo VARCHAR(100),
-   Correo VARCHAR(255) UNIQUE,
-   Contraseña VARCHAR(255),
-   Rol VARCHAR(100)
+CREATE TABLE tipo_cliente (
+   id_tipo_cliente INT AUTO_INCREMENT PRIMARY KEY,
+   Descripcion VARCHAR(100) 
 );
 
 CREATE TABLE cliente (
    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-   Nombre_cliente VARCHAR(100),
-   DNI VARCHAR(20),
-   Direccion VARCHAR(255),
-   Correo VARCHAR(255),
-   Telefono VARCHAR(20)
+   id_tipo_cliente INT,
+   Nombre VARCHAR(50), 
+   Cuil_cuit VARCHAR(11), 
+   Direccion VARCHAR(150), 
+   Correo VARCHAR(50), 
+   Telefono VARCHAR(15), 
+   FOREIGN KEY (id_tipo_cliente) REFERENCES tipo_cliente(id_tipo_cliente)
 );
 
 CREATE TABLE empleado (
    id_empleado INT AUTO_INCREMENT PRIMARY KEY,
-   id_usuario INT,
-   Nombre VARCHAR(100),
-   Apellido VARCHAR(100),
-   DNI VARCHAR(20) UNIQUE,
-   Correo VARCHAR(255) UNIQUE,
-   Telefono VARCHAR(20),
-   id_rol INT,
-   Salario VARCHAR(20),
+   Usuario INT,
+   Nombre VARCHAR(50), 
+   Apellido VARCHAR(50), 
+   DNI VARCHAR(8) UNIQUE, 
+   Correo VARCHAR(50) UNIQUE,
+   Telefono VARCHAR(15), 
+   Rol INT,
+   Salario DECIMAL(10,2), 
    Fecha_Contratacion DATE,
-   FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-   FOREIGN KEY (id_rol) REFERENCES roles(id_rol)
+   Contraseña VARCHAR(50),
+   FOREIGN KEY (Rol) REFERENCES roles(id_rol)
 );
 
-CREATE TABLE requisito_cliente (
-   id_requisito INT AUTO_INCREMENT PRIMARY KEY,
-   Fecha DATE,
-   Descripcion VARCHAR(400),
-   id_cliente INT,
-   id_empleado INT,
-   FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
-   FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
-);
-
-CREATE TABLE estado_proyecto (
+CREATE TABLE estado (
    id_estado INT AUTO_INCREMENT PRIMARY KEY,
-   Estado VARCHAR(100)
+   Descripcion VARCHAR(50) 
+);
+
+CREATE TABLE estado_pago (
+   id_estado_pago INT AUTO_INCREMENT PRIMARY KEY,
+   estado_pago VARCHAR(50) 
+);
+
+CREATE TABLE metodo_pago (
+   id_metodo_pago INT AUTO_INCREMENT PRIMARY KEY,
+   Descripcion VARCHAR(50) 
 );
 
 CREATE TABLE pago (
    id_pago INT AUTO_INCREMENT PRIMARY KEY,
    Fecha DATE,
-   Monto INT,
-   Metodo_pago VARCHAR(100),
-   Estado VARCHAR(100)
+   Monto DECIMAL(10,2), 
+   Metodo_pago INT,
+   Estado_pago INT,
+   FOREIGN KEY (Estado_pago) REFERENCES estado_pago(id_estado_pago),
+   FOREIGN KEY (Metodo_pago) REFERENCES metodo_pago(id_metodo_pago)
 );
 
 CREATE TABLE proyecto (
    id_proyecto INT AUTO_INCREMENT PRIMARY KEY,
-   Nombre VARCHAR(100),
-   Descripcion VARCHAR(255),
+   Nombre VARCHAR(100), 
+   Descripcion VARCHAR(200), 
    Fecha_inicio DATE,
    Fecha_final DATE,
    Fecha_final_real DATE,
-   Gerente_proyecto INT,
-   Desarrollador INT,
-   id_requisito INT,
+   id_cliente INT,
    id_estado INT,
    id_pago INT,
    FOREIGN KEY (id_pago) REFERENCES pago(id_pago),
-   FOREIGN KEY (Gerente_proyecto) REFERENCES empleado(id_empleado),
-   FOREIGN KEY (Desarrollador) REFERENCES empleado(id_empleado),
-   FOREIGN KEY (id_estado) REFERENCES estado_proyecto(id_estado)
+   FOREIGN KEY (id_estado) REFERENCES estado(id_estado),
+   FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+);
+
+CREATE TABLE prioridad (
+   id_prioridad INT AUTO_INCREMENT PRIMARY KEY,
+   Descripcion VARCHAR(50) 
+);
+
+CREATE TABLE requisitos (
+   id_requisito INT AUTO_INCREMENT PRIMARY KEY,
+   id_proyecto INT,
+   Descripcion VARCHAR(200),
+   Fecha_inicial DATE,
+   Fecha_final DATE,
+   Fecha_final_real DATE,
+   id_empleado INT,
+   id_prioridad INT,
+   id_estado INT,
+   FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto),
+   FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado),
+   FOREIGN KEY (id_prioridad) REFERENCES prioridad(id_prioridad),
+   FOREIGN KEY (id_estado) REFERENCES estado(id_estado)
 );
--- Pages/register.php
+++ Pages/register.php
@@ -1,62 +1,8 @@
 <?php
-require_once('C:\xampp\htdocs\UCH\BASE_DE_DATOS\CONSULTORA_SOFTWARE\DataBase\connection.php');
+include("C:\xampp\htdocs\UCH\BASE_DE_DATOS\CONSULTORA_SOFTWARE\DataBase");
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    // Obtener datos del formulario
-    $nombre_completo = $conn->real_escape_string($_POST['nombre_completo']);
-    $correo = $conn->real_escape_string($_POST['correo']);
-    $usuario = $conn->real_escape_string($_POST['usuario']);
-    $contraseña = password_hash($conn->real_escape_string($_POST['contraseña']), PASSWORD_DEFAULT);
-    $rol = $conn->real_escape_string($_POST['rol']);
 
-    // Insertar datos en la tabla usuario
-    $sql = "INSERT INTO usuario (Nombre_completo, Correo, Usuario, Contraseña, Rol) VALUES ('$nombre_completo','$correo','$usuario', '$contraseña', '$rol')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Registro exitoso. ";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
 
-// Cerrar conexión
-$conn->close();
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Registro de Usuario</title>
-    <link rel="stylesheet" type="text/css" href="../Css/style.css">
-</head>
-<body>
-    <h2>Registro de Usuario</h2>
-    <div class="form-container">
-        <form method="POST" action="register.php">
-            <label>Nombre Completo:</label>
-            <input type="text" name="nombre_completo" required>
-
-            <label>Correo:</label>
-            <input type="email" name="correo" required>
-
-            <label>Usuario:</label>
-            <input type="text" name="usuario" required>
-
-            <label>Contraseña:</label>
-            <input type="password" name="contraseña" required>
-
-            <label>Rol:</label>
-            <select name="rol" required>
-                <option value="" selected disabled>Seleccione un rol</option>
-                <option value="Gerente de Proyecto">Gerente de Proyecto</option>
-                <option value="Desarrollador">Desarrollador</option>
-                <option value="Consultor">Consultor</option>
-            </select>
-
-            <button type="submit">Registrar</button>
-            <a href='login.php'>Iniciar sesión</a>
-        </form>
-    </div>
-</body>
-</html>
