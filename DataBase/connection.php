4b7f3a181dce1d00b60fddd08b801e3b051c6bdb
artumarinn <artumarin.09@gmail.com>
2024-06-23 23:47:57 -0300

    Conexion de la bd

--- DataBase/connection.php
+++ DataBase/connection.php
@@ -1,7 +1,7 @@
 <?php
 $conn = new mysqli('localhost', 'root', 'marin', 'sistemaconsultora');
 
-// Verificar conexión
+// Verificar conexión bd
 if ($conn->connect_error) {
     die('Conexión fallida: ' . $conn->connect_error);
 }
